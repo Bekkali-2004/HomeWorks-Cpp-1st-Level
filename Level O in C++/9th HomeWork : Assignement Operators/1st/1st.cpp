3e4a5e51a4cdfac16af3da1607ccba13226f9b0c
dascofire <modistotube2004@gmail.com>
2024-11-14 13:46:24 +0100

    Change it

---
+++ Level O in C++/9th HomeWork : Assignement Operators/1st/1st.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int a = 10 , b = 20;
+
+    a += b; // a + b
+    cout << a << endl;
+    a -= b; // a - b
+    cout << a << endl;
+    a *= b; // a * b
+    cout << a << endl;
+    a /= b; // a / b
+    cout << a << endl;
+    a %= b; // a % b
+    cout << a << endl;
+
+    return 0;
+}
