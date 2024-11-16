4949e8b6e8ec2fa04d68b5aeb79d4b7d175e4069
dascofire <modistotube2004@gmail.com>
2024-11-16 14:16:04 +0100

    .......

---
+++ Level O in C++/10th HomWork : Relatrional Operators/2nd/2nd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int A, B;
+
+    cout << "Enter The 1st Number A : " << endl;
+    cin >> A;
+    cout << "Enter The 2nd Number B : " << endl;
+    cin >> B;
+
+    cout << "-----------------------------------------" << endl;
+
+    cout << (A == B) << endl;
+    cout << (A != B) << endl;
+    cout << (A >= B) << endl;
+    cout << (A <= B) << endl;
+    cout << (A > B) << endl;
+    cout << (A < B) << endl;
+
+    return 0;
+}
