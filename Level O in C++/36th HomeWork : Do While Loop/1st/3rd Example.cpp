8498b000a45e61f9ac6c124dfdd8ffb63ade28b8
Bekkali <111304412+Bekkali-2004@users.noreply.github.com>
2025-02-04 12:38:02 +0100

    Do While Loop

---
+++ Level O in C++/36th HomeWork : Do While Loop/1st/3rd Example.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+using namespace std;
+
+int ReadRangeNumber(int From, int To){
+    int Number;
+
+    cout << "Enter a number between " << From << " and " << To << ": " << endl;
+    cin >> Number;
+
+    do
+    {
+        cout << "Wrong number, please try again: " << endl;
+        cout << "Enter a number between " << From << " and " << To << ": " << endl;
+        cin >> Number;
+    }while (Number < From || Number > To);
+    
+    return Number;
+}
+
+int main(){
+
+    int From = 1, To = 100;
+
+    cout << "The Number is : " << ReadRangeNumber(From, To) << endl;
+}
